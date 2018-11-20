b0c008a677c462e80366757c6e16233d4a517589
Ahmed Abduljabbar <aabduljabbar1@student.sierracollege.edu>
2018-11-20 13:04:14 -0800

    "Added Copyright window"

---
+++ src/aabduljabbar1/CopyRightBox.java
@@ -0,0 +1,36 @@
+package aabduljabbar1;
+
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+
+public class CopyRightBox {
+
+    public static void display(String title, String message) {
+        Stage window = new Stage();
+
+        //Block events to other windows
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(300);
+        window.setHeight(200);
+
+        Label label = new Label();
+        label.setText(message);
+        Button closeButton = new Button("Close");
+        closeButton.setOnAction(e -> window.close());
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, closeButton);
+        layout.setAlignment(Pos.CENTER);
+
+        //Display window and wait for it to be closed before returning
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+    }
+
+}
+
