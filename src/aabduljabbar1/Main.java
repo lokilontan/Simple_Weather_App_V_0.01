68661c340071437ffccd7a4bf61660c0eb1789fb
Ahmed Abduljabbar <aabduljabbar1@student.sierracollege.edu>
2018-11-25 18:24:35 -0800

    "Modified the GUI"

--- src/aabduljabbar1/Main.java
+++ src/aabduljabbar1/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Simple Weather App V.01");
+        primaryStage.setTitle("Simple Weather App V.03");
         primaryStage.setScene(new Scene(root, 765, 520));
         primaryStage.show();
         primaryStage.setResizable(false);
