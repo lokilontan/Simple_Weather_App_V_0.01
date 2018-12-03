0a8880273b31bf8294923a3b5225908aad9b3d12
Vladimir Plagov <lokilontan@yandex.ru>
2018-12-03 11:21:02 -0800

    ASync Completely done

--- src/aabduljabbar1/Controller.java
+++ src/aabduljabbar1/Controller.java
@@ -1,6 +1,7 @@
 package aabduljabbar1;
 
 
+import com.google.gson.JsonElement;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -149,6 +150,8 @@ public class Controller {
 
     CurrentWeather CW = new CurrentWeather();
 
+    //static Weather W;
+
 
     public void handleListView() {
         zipField.setText(listCities.getSelectionModel().getSelectedItem().toString());
@@ -244,10 +247,10 @@ public class Controller {
         }
     }
 
-    public void handleWeatherButton(ActionEvent e)
-    {
+    public void handleWeatherButton(ActionEvent e) {
 
         Image loading = new Image(Main.class.getResourceAsStream("loading.gif"));
+
 
         loadImgView.setImage(loading);
         loadImgView.setFitWidth(100);
@@ -257,11 +260,96 @@ public class Controller {
         loadImgView.relocate(65, 225);
         loadImgView.setVisible(true);
 
-        AsyncTask t = new GetDataInBackground();
-        t.execute(zipField.getText());
+
+         AsyncTask t = new GetDataInBackground();
+         t.execute(zipField.getText());
+
+      /*//  W = new Weather(zipField.getText());
+
+        W.setDaemon(true);
+        W.start();
 
 
+     //  W.fetch();
+       // W.fetchRS();
+
+
+        try {
+
+            System.out.println(W.getImageString(W.DynamicJson));
+            System.out.println(W.getImageString(W.DJ2));
+            Image imgCondition = new Image(W.getImageString(W.DynamicJson));
+            imgView.setImage(imgCondition);
+            imgView.setVisible(true);
+
+
+            Image imgRadar = new Image(W.getRadarImg(W.RSJson));
+            radarImg.setImage(imgRadar);
+            radarImg.setVisible(true);
+
+            Image imgSat = new Image(W.getSatImg(W.RSJson));
+            satImg.setImage(imgSat);
+            satImg.setVisible(false);
+
+
+            loc.setText(W.getCityState(W.DynamicJson));
+            loc.setVisible(true);
+            con.setText(W.getWeather(W.DynamicJson));
+            con.setVisible(true);
+            temF.setText(W.getTemperatureF(W.DynamicJson));
+            temF.setVisible(true);
+            temC.setText(W.getTemperatureC(W.DynamicJson));
+            temC.setVisible(true);
+            temC1.setText(W.getTemperatureC(W.DynamicJson));
+            temC1.setVisible(true);
+            temF1.setText(W.getTemperatureF(W.DynamicJson));
+            temF1.setVisible(false);
+            windDir.setText(W.getWindDir(W.DynamicJson));
+            windDir.setVisible(true);
+            humidity.setText(W.getHumidity((W.DynamicJson)));
+            humidity.setVisible(true);
+            line.setVisible(true);
+
+            //FORECAST
+
+            //DAY 1
+            W.getLocationDay(W.DynamicJson, 0, day0, imgViewDay0, conDay0, highTempFDay0,
+                    lowTempFDay0, highTempCDay0, lowTempCDay0);
+
+            //DAY 2
+            W.getLocationDay(W.DynamicJson, 1, day1, imgViewDay1, conDay1, highTempFDay1,
+                    lowTempFDay1, highTempCDay1, lowTempCDay1);
+
+            //DAY 3
+            W.getLocationDay(W.DynamicJson, 2, day2, imgViewDay2, conDay2, highTempFDay2,
+                    lowTempFDay2, highTempCDay2, lowTempCDay2);
+
+            //DAY 4
+            W.getLocationDay(W.DynamicJson, 3, day3, imgViewDay3, conDay3, highTempFDay3,
+                    lowTempFDay3, highTempCDay3, lowTempCDay3);
+
+            //DAY 5
+            W.getLocationDay(W.DynamicJson, 4, day4, imgViewDay4, conDay4, highTempFDay4,
+                    lowTempFDay4, highTempCDay4, lowTempCDay4);
+
+            //DAY 6
+            W.getLocationDay(W.DynamicJson, 5, day5, imgViewDay5, conDay5, highTempFDay5,
+                    lowTempFDay5, highTempCDay5, lowTempCDay5);
+
+            //DAY 7
+            W.getLocationDay(W.DynamicJson, 6, day6, imgViewDay6, conDay6, highTempFDay6,
+                    lowTempFDay6, highTempCDay6, lowTempCDay6);
+
+            loadImgView.setVisible(false);
+
+        } catch (NullPointerException nue) {
+            CopyRightBox.display("Error", "Something went wrong (NullPointerException). Try again!");
+        }
+
+        //W.stop();*/
     }
+
+
 
     public void handleClearButton(ActionEvent e)
     {
--- src/aabduljabbar1/Weather.java
+++ src/aabduljabbar1/Weather.java
@@ -14,155 +14,167 @@ import java.net.URL;
 
 
 public class Weather {
-    private String zip, city, state;
-    public JsonElement DynamicJson; //json for any weather call
-    public JsonElement RSJson;
+
+
+            private String zip, city, state;
+            public JsonElement DynamicJson; //json for any weather call
+            public JsonElement DJ2;
+            public JsonElement RSJson;
 
 
     public Weather(String zipCode) {
-        zip = zipCode;
+                zip = zipCode;
 
-        if (zip.contains("-")) {
-            zip = zip.substring(0, 5);
-        } else if (zip.contains(",")) {
-            int i = zip.indexOf(",");
-            city = zip.substring(0, i);
-            state = zip.substring(i + 2, zip.length());
-            zip = state + "/" + city;
-            zip = zip.replace(" ", "%20");
-        } else zip = zip.replace(" ", "%20");
-    }
+                if (zip.contains("-")) {
+                    zip = zip.substring(0, 5);
+                } else if (zip.contains(",")) {
+                    int i = zip.indexOf(",");
+                    city = zip.substring(0, i);
+                    state = zip.substring(i + 2, zip.length());
+                    zip = state + "/" + city;
+                    zip = zip.replace(" ", "%20");
+                } else zip = zip.replace(" ", "%20");
+            }
 
-    public void getLocationDay(JsonElement j, int dayN, Label dayDate, ImageView dayImgView, Label dayCond,
-                               Label dayHTempF, Label dayLTempF, Label dayHTempC, Label dayLTempC) {
+          /*  public void run() {
+        fetch();
+        fetchRS();
 
-        dayDate.setText(j.getAsJsonObject().get("forecast").getAsJsonObject().get("simpleforecast").getAsJsonObject()
-                .get("forecastday").getAsJsonArray().get(dayN).getAsJsonObject().get("date").getAsJsonObject()
-                .get("weekday").getAsString());
+            }*/
 
-        Image dayImgCondition = new Image(j.getAsJsonObject().get("forecast").getAsJsonObject().get("simpleforecast").getAsJsonObject()
-                .get("forecastday").getAsJsonArray().get(dayN).getAsJsonObject().get("icon_url").getAsString());
+                public void getLocationDay (JsonElement j,int dayN, Label dayDate, ImageView dayImgView, Label dayCond,
+                        Label dayHTempF, Label dayLTempF, Label dayHTempC, Label dayLTempC){
 
-        dayImgView.setImage(dayImgCondition);
+                    dayDate.setText(j.getAsJsonObject().get("forecast").getAsJsonObject().get("simpleforecast").getAsJsonObject()
+                            .get("forecastday").getAsJsonArray().get(dayN).getAsJsonObject().get("date").getAsJsonObject()
+                            .get("weekday").getAsString());
 
-        dayCond.setText(j.getAsJsonObject().get("forecast").getAsJsonObject().get("simpleforecast").getAsJsonObject()
-                .get("forecastday").getAsJsonArray().get(dayN).getAsJsonObject().get("conditions").getAsString());
+                    Image dayImgCondition = new Image(j.getAsJsonObject().get("forecast").getAsJsonObject().get("simpleforecast").getAsJsonObject()
+                            .get("forecastday").getAsJsonArray().get(dayN).getAsJsonObject().get("icon_url").getAsString());
 
-        dayCond.setVisible(true);
+                    dayImgView.setImage(dayImgCondition);
 
-        dayHTempF.setText(j.getAsJsonObject().get("forecast").getAsJsonObject().get("simpleforecast").getAsJsonObject()
-                .get("forecastday").getAsJsonArray().get(dayN).getAsJsonObject().get("high").getAsJsonObject()
-                .get("fahrenheit").getAsString());
+                    dayCond.setText(j.getAsJsonObject().get("forecast").getAsJsonObject().get("simpleforecast").getAsJsonObject()
+                            .get("forecastday").getAsJsonArray().get(dayN).getAsJsonObject().get("conditions").getAsString());
 
-        dayHTempF.setVisible(false);
+                    dayCond.setVisible(true);
 
-        dayHTempC.setText(j.getAsJsonObject().get("forecast").getAsJsonObject().get("simpleforecast").getAsJsonObject()
-                .get("forecastday").getAsJsonArray().get(dayN).getAsJsonObject().get("high").getAsJsonObject()
-                .get("celsius").getAsString());
+                    dayHTempF.setText(j.getAsJsonObject().get("forecast").getAsJsonObject().get("simpleforecast").getAsJsonObject()
+                            .get("forecastday").getAsJsonArray().get(dayN).getAsJsonObject().get("high").getAsJsonObject()
+                            .get("fahrenheit").getAsString());
 
-        dayHTempC.setVisible(true);
+                    dayHTempF.setVisible(false);
 
-        dayLTempF.setText(j.getAsJsonObject().get("forecast").getAsJsonObject().get("simpleforecast").getAsJsonObject()
-                .get("forecastday").getAsJsonArray().get(dayN).getAsJsonObject().get("low").getAsJsonObject()
-                .get("fahrenheit").getAsString());
+                    dayHTempC.setText(j.getAsJsonObject().get("forecast").getAsJsonObject().get("simpleforecast").getAsJsonObject()
+                            .get("forecastday").getAsJsonArray().get(dayN).getAsJsonObject().get("high").getAsJsonObject()
+                            .get("celsius").getAsString());
 
-        dayLTempF.setVisible(false);
+                    dayHTempC.setVisible(true);
 
-        dayLTempC.setText(j.getAsJsonObject().get("forecast").getAsJsonObject().get("simpleforecast").getAsJsonObject()
-                .get("forecastday").getAsJsonArray().get(dayN).getAsJsonObject().get("low").getAsJsonObject()
-                .get("celsius").getAsString());
+                    dayLTempF.setText(j.getAsJsonObject().get("forecast").getAsJsonObject().get("simpleforecast").getAsJsonObject()
+                            .get("forecastday").getAsJsonArray().get(dayN).getAsJsonObject().get("low").getAsJsonObject()
+                            .get("fahrenheit").getAsString());
 
-        dayLTempC.setVisible(true);
+                    dayLTempF.setVisible(false);
 
-    }
+                    dayLTempC.setText(j.getAsJsonObject().get("forecast").getAsJsonObject().get("simpleforecast").getAsJsonObject()
+                            .get("forecastday").getAsJsonArray().get(dayN).getAsJsonObject().get("low").getAsJsonObject()
+                            .get("celsius").getAsString());
+
+                    dayLTempC.setVisible(true);
+
+                }
 
 
-    public String getImageString(JsonElement j) {
-        return j.getAsJsonObject().get("current_observation").getAsJsonObject()
-                .get("icon_url").getAsString();
-    }
+                public String getImageString (JsonElement j){
+                    return j.getAsJsonObject().get("current_observation").getAsJsonObject()
+                            .get("icon_url").getAsString();
+                }
 
 
-    public String getCityState(JsonElement j) {
-        return j.getAsJsonObject().get("current_observation").getAsJsonObject()
-                .get("display_location").getAsJsonObject()
-                .get("full").getAsString();
-    }
+                public String getCityState (JsonElement j){
+                    return j.getAsJsonObject().get("current_observation").getAsJsonObject()
+                            .get("display_location").getAsJsonObject()
+                            .get("full").getAsString();
+                }
 
-    public String getTemperatureF(JsonElement j) {
-        return j.getAsJsonObject().get("current_observation").getAsJsonObject()
-                .get("temp_f").getAsString() + "° F";
-    }
+                public String getTemperatureF (JsonElement j){
+                    return j.getAsJsonObject().get("current_observation").getAsJsonObject()
+                            .get("temp_f").getAsString() + "° F";
+                }
 
-    public String getTemperatureC(JsonElement j) {
-        return j.getAsJsonObject().get("current_observation").getAsJsonObject()
-                .get("temp_c").getAsString() + "° C";
-    }
+                public String getTemperatureC (JsonElement j){
+                    return j.getAsJsonObject().get("current_observation").getAsJsonObject()
+                            .get("temp_c").getAsString() + "° C";
+                }
 
-    public String getWeather(JsonElement j) {
-        return j.getAsJsonObject().get("current_observation").getAsJsonObject()
-                .get("weather").getAsString();
-    }
+                public String getWeather (JsonElement j){
+                    return j.getAsJsonObject().get("current_observation").getAsJsonObject()
+                            .get("weather").getAsString();
+                }
 
-    public String getWindDir(JsonElement j) {
-        return "Wind " + j.getAsJsonObject().get("current_observation").getAsJsonObject()
-                .get("wind_dir").getAsString();
-    }
+                public String getWindDir (JsonElement j){
+                    return "Wind " + j.getAsJsonObject().get("current_observation").getAsJsonObject()
+                            .get("wind_dir").getAsString();
+                }
 
-    public String getHumidity(JsonElement j) {
-        return "Humidity " + j.getAsJsonObject().get("current_observation").getAsJsonObject()
-                .get("relative_humidity").getAsString();
-    }
+                public String getHumidity (JsonElement j){
+                    return "Humidity " + j.getAsJsonObject().get("current_observation").getAsJsonObject()
+                            .get("relative_humidity").getAsString();
+                }
 
-    public String getRadarImg(JsonElement j) {
-        return j.getAsJsonObject().get("radar").getAsJsonObject()
-                .get("image_url").getAsString();
-    }
+                public String getRadarImg (JsonElement j){
+                    return j.getAsJsonObject().get("radar").getAsJsonObject()
+                            .get("image_url").getAsString();
+                }
 
-    public String getSatImg(JsonElement j){
-        return j.getAsJsonObject().get("satellite").getAsJsonObject()
-                .get("image_url").getAsString();
-    }
+                public String getSatImg (JsonElement j){
+                    return j.getAsJsonObject().get("satellite").getAsJsonObject()
+                            .get("image_url").getAsString();
+                }
 
-    public void fetch() {
-        String wdRequest = "http://api.wunderground.com/api/1655f919bbcd29ed/conditions/forecast7day/q/" + zip + ".json";
+                public void fetch () {
+                    String wdRequest = "http://api.wunderground.com/api/1655f919bbcd29ed/conditions/forecast7day/q/" + zip + ".json";
 
-        try {
-            URL wdURL = new URL(wdRequest);
+                    try {
+                        URL wdURL = new URL(wdRequest);
 
-            InputStream is = wdURL.openStream();
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
+                        InputStream is = wdURL.openStream();
+                        InputStreamReader isr = new InputStreamReader(is);
+                        BufferedReader br = new BufferedReader(isr);
 
-            JsonParser parser = new JsonParser();
-            DynamicJson = parser.parse(br);
-        } catch (java.net.MalformedURLException mue) {
-            System.out.println("URL not valid");
-            System.exit(1);
-        } catch (java.io.IOException ioe) {
-            System.out.println("IO Exception Caught");
-            System.exit(1);
-        }
-    }
+                        JsonParser parser = new JsonParser();
+                        DynamicJson = parser.parse(br);
+                    } catch (java.net.MalformedURLException mue) {
+                        System.out.println("URL not valid");
+                        System.exit(1);
+                    } catch (java.io.IOException ioe) {
+                        System.out.println("IO Exception Caught");
+                        System.exit(1);
+                    }
+                }
 
-    public void fetchRS() {
-        String wdRequest = "http://api.wunderground.com/api/1655f919bbcd29ed/radar/satellite/q/" + zip + ".json";
+                public void fetchRS () {
+                    String wdRequest = "http://api.wunderground.com/api/1655f919bbcd29ed/radar/satellite/q/" + zip + ".json";
 
-        try {
-            URL wdURL = new URL(wdRequest);
+                    try {
+                        URL wdURL = new URL(wdRequest);
 
-            InputStream is = wdURL.openStream();
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
+                        InputStream is = wdURL.openStream();
+                        InputStreamReader isr = new InputStreamReader(is);
+                        BufferedReader br = new BufferedReader(isr);
 
-            JsonParser parser = new JsonParser();
-            RSJson = parser.parse(br);
-        } catch (java.net.MalformedURLException mue) {
-            System.out.println("URL not valid");
-            System.exit(1);
-        } catch (java.io.IOException ioe) {
-            System.out.println("IO Exception Caught");
-            System.exit(1);
-        }
-    }
+                        JsonParser parser = new JsonParser();
+                        RSJson = parser.parse(br);
+                    } catch (java.net.MalformedURLException mue) {
+                        System.out.println("URL not valid");
+                        System.exit(1);
+                    } catch (java.io.IOException ioe) {
+                        System.out.println("IO Exception Caught");
+                        System.exit(1);
+                    }
+                }
+
+
+
 }
